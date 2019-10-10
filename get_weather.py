3da6216ab5b34379f396c4fdc23c092256712076
Cathy Zheng <czheng@zynga.com>
2019-10-10 11:40:40 -0700

    Add foster city

--- get_weather.py
+++ get_weather.py
@@ -1,14 +1,18 @@
 import requests, json 
 import streamlit as st
+from datetime import date
 
-st.title("Daily Commute")
+st.title("Good Morning!")
+current_date = date.today()
+st.markdown("Today is "+str(current_date)+", "+current_date.strftime('%A'))
 st.markdown("## Weather")
 
 api_key = "ac78d812f2287fd1958216aff7f1957d"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-id = "5391997"
-city_name = "San Francisco"
-complete_url = base_url + "appid=" + api_key + "&id=" + id + "&units=metric"
+base_url = "http://api.openweathermap.org/data/2.5/group?"
+
+city_dict = {"San Francisco":"5391997", "Foster City":"5350159"}
+ids = ','.join(city_dict.values())
+complete_url = base_url + "appid=" + api_key + "&id=" + ids + "&units=metric"
 
 @st.cache
 def fetch_api(url):
@@ -16,19 +20,16 @@ def fetch_api(url):
     x = response.json()
     return x 
 
-x = fetch_api(complete_url)
-if x["cod"] == "404": 
-    " City Not Found "
-elif x["cod"] == "429": 
-    " Account is temporary blocked "
-else: 
+city_weather = fetch_api(complete_url)
+for x in city_weather['list']:
     y = x["main"] 
     current_temperature = y["temp"] 
     max_temperature = y["temp_max"] 
     min_temperature = y["temp_min"] 
     z = x["weather"]
     weather_description = z[0]["description"]
-    st.markdown("San Francisco has " + str(weather_description))
-    st.markdown("Current temperature is " + str(current_temperature))
-    st.markdown("Highest temperature is " + str(max_temperature))
-    st.markdown("Lowest temperature is " + str(min_temperature))
+    city = x['name']
+    st.markdown(city + " has " + str(weather_description))
+    st.markdown("Currently at " + str(current_temperature) + "&deg;C")
+    st.markdown("With highest at " + str(max_temperature) + "&deg;C")
+    st.markdown("and lowest at " + str(min_temperature) + "&deg;C")
